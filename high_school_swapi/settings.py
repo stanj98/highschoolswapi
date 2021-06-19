a8bd9c4f616f5d0b8519cec62e80a27056126640
Stanley John <stanleyjohn98@gmail.com>
2021-06-20 01:35:59 +0530

    updated settings

--- high_school_swapi/settings.py
+++ high_school_swapi/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%5x9yy5kno@3pfueqtmt55k!pwrosu2hq&h1&=dyi)yugn+6)z'
+# SECRET_KEY = '%5x9yy5kno@3pfueqtmt55k!pwrosu2hq&h1&=dyi)yugn+6)z'
+SECRET_KEY = os.environ.get('swapiSecretKey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
